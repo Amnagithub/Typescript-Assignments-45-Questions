96f0d999e64cc373ab853b0498360c479e5e66c3
Amnagithub <amnaaplus1@gmail.com>
2024-03-03 22:43:55 +0500

    first commit

---
+++ Question#5/app.js
@@ -0,0 +1,9 @@
+// Q5:Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// store a famous_person name:
+var famous_person = ("Federick Douglass");
+// store a Quote :
+var Quote = "Its easy to build a strong children then to repair a broken man.";
+// store a Message :
+var Message = "".concat(famous_person, " once said, \"").concat(Quote, "\"");
+// print Message :
+console.log(Message);
---
+++ Question#5/app.ts
@@ -0,0 +1,23 @@
+// Q5:Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+// store a famous_person name:
+let famous_person : string = ("Federick Douglass");
+
+// store a Quote :
+
+let Quote : string = "Its easy to build a strong children then to repair a broken man.";
+
+// store a Message :
+
+let Message : string = `${famous_person} once said, "${Quote}"`;
+
+// print Message :
+
+console.log(Message);
+
+
+
+
+
+
+
