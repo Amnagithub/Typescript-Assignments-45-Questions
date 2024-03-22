08fa0c044e83420ab1502325afba49faeacbd046
Amnagithub <amnaaplus1@gmail.com>
2024-03-22 22:07:49 +0500

    first commit

---
+++ Question#11/app.ts
@@ -0,0 +1,11 @@
+// Q11:Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// store few friends name in a Array
+
+var names = ['shabana', 'ghazala', 'noreen', 'amber'];
+
+// print each names 
+
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
