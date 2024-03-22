6af247c093b6932e957bcee362a39dd6bbfd3570
Amnagithub <amnaaplus1@gmail.com>
2024-03-22 22:41:15 +0500

    first commit

---
+++ Question#12/app.js
@@ -0,0 +1,9 @@
+// Q12:Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+// make a Array
+var names = ['Shabana', 'Ghazala', 'Noreen', 'Amber'];
+// creat a massage
+var message1 = " You are invited a dinner at Pearl Continantal Hotel tomorrow.";
+// print message with the each person's name
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i] + message1);
+}
---
+++ Question#12/app.ts
@@ -0,0 +1,14 @@
+// Q12:Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+// make a Array
+
+let names : string[] = ['Shabana','Ghazala','Noreen','Amber'];
+
+// creat a massage
+
+let message1 : string = " You are invited a dinner at Pearl Continantal Hotel tomorrow.";
+
+// print message with the each person's name
+
+for (let i = 0; i < names.length; i++){
+  console.log(names[i] + message1);
+}
