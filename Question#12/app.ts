2672569f5425d1125eb6149291ca082feffaf73a
Amnagithub <amnaaplus1@gmail.com>
2024-03-22 22:47:42 +0500

    updated

--- Question#12/app.ts
+++ Question#12/app.ts
@@ -1,4 +1,5 @@
 // Q12:Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
 // make a Array
 
 let names : string[] = ['Shabana','Ghazala','Noreen','Amber'];
